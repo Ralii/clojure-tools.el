b6059a0f35ae837bcb4e1581c4e16819212ce0e6
lari.saukkone <lari.saukkonean1@gmail.com>
2023-09-16 17:04:38 +0300

    alter el file

--- clojure-tools.el
+++ clojure-tools.el
@@ -1,15 +1,6 @@
 ;;; clojure-tools.el -*- lexical-binding: t; -*-
 ;;
-;; Copyright (C) 2023 John Doe
-;;
-;; Author: John Doe <john@doe.com>
-;; Maintainer: John Doe <john@doe.com>
-;; Created: Syyskuu 16, 2023
-;; Modified: Syyskuu 16, 2023
-;; Version: 0.0.1
-;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
-;; Homepage: https://github.com/lari/clojure-tools
-;; Package-Requires: ((emacs "24.3"))
+;; Copyright (C) 2023 Lari Saukkonen
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
