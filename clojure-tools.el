9360c0a218220d63c7f240a8af9e12e5d8838a95
lari.saukkone <lari.saukkonean1@gmail.com>
2023-09-16 15:49:17 +0300

    add tools and readme

---
+++ clojure-tools.el
@@ -0,0 +1,91 @@
+;;; clojure-tools.el -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2023 John Doe
+;;
+;; Author: John Doe <john@doe.com>
+;; Maintainer: John Doe <john@doe.com>
+;; Created: Syyskuu 16, 2023
+;; Modified: Syyskuu 16, 2023
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/lari/clojure-tools
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;;; Commentary:
+;; Small tools related to Clojure/Clojurescript development and debugging
+;;
+;;
+;;; Code:
+(require 'cider)
+
+(defun clojure-tools-malli-data->schema ()
+  "requires malli to be installed"
+  (interactive)
+  (cider-interactive-eval (format "(require 'malli.provider)
+                                   (malli.provider/provide %s)"
+                                  (cider-last-sexp))
+                          (cider-eval-print-handler)
+                          nil
+                          (cider--nrepl-pr-request-map)))
+
+(defun clojure-tools-malli-schema->data ()
+  "requires malli to be installed"
+  (interactive)
+  (cider-interactive-eval (format "(require 'malli.generator)
+                                   (malli.generator/sample %s {:size 5 :seed 20})"
+                                  (cider-last-sexp))
+                          (cider-eval-print-handler)
+                          nil
+                          (cider--nrepl-pr-request-map)))
+
+(defun clojure-tools-force-render! ()
+  "Forces render on only evaluated components. Much faster than hot-reload"
+  (interactive)
+  (cider-interactive-eval "(iprally-ui.routes/render-app)"
+                          nil
+                          nil
+                          (cider--nrepl-pr-request-map)))
+
+(defun clojure-tools-eval-and-force-render! ()
+  "Evaluates function under point and renders it to screen"
+  (interactive)
+  (cider-eval-defun-at-point)
+  (clojure-tools-force-render!))
+
+(defun clojure-tools-inspect-app-db ()
+  "Open re-frame global state in cider inspector"
+  (interactive)
+  (cider-inspect-expr "@re-frame.db/app-db" (cider-current-ns)))
+
+(defun clojure-tools-lr-snitch-clj ()
+  "Init snitch (requires snitch)"
+  (interactive)
+  (cider-interactive-eval "(doseq [ns (all-ns)]
+                                (binding [*ns* ns]
+                                (require '[snitch.core :refer [defn* *fn *let]])))"
+                          nil
+                          nil
+                          (cider--nrepl-pr-request-map)))
+
+(defun clojure-tools-cycle-defn* ()
+  (interactive)
+  (save-excursion
+    (ignore-errors (forward-char 7))
+    (search-backward-regexp "(defn?\\(*\\| ^:private\\)?\\_>")
+    (if (match-string 1)
+        (replace-match "" nil nil nil 1)
+      (goto-char (match-end 0))
+      (insert "*"))))
+
+(defun clojure-tools-subscribe-under-point ()
+  "Will subscribe reg-sub under point and check it inside inspector"
+  (interactive)
+  (cider-inspect-expr (format "(do (require 're-frame.core)
+                               @(re-frame.core/subscribe [%s]))"
+                              (cider-sexp-at-point))
+                      (cider-current-ns)))
+
+(provide 'clojure-tools)
+;;; clojure-tools.el ends here
